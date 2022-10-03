a2beda36008e9a33fa4906f747a39acef57f4f61
emanmoq <42995029+emanmoq@users.noreply.github.com>
2022-10-03 02:14:49 -0700

    Ar

---
+++ assets/js/mainAr.js
@@ -0,0 +1,346 @@
+jQuery(function ($) {
+    $(".modal").modal("show")
+  
+    $(".heroSlider").owlCarousel({
+      nav: false,
+      loop: true,
+      dots: false,
+      items: 1,
+      rtl:true,
+  
+    });
+    $(".InstructorsSlider").owlCarousel({
+      nav: true,
+      loop: false,
+      dots: true,
+      items: 4,
+      rtl:true,
+      navText: ['<i class="fa fa-chevron-left"></i>', '<i class="fa fa-chevron-right"></i>'],
+      margin: 70,
+      responsive: {
+        0: {
+          items: 1
+        },
+        700: {
+          items: 2
+        },
+        1000: {
+          items: 4,
+        },
+  
+      }
+    });
+    $(".partnerSlider").owlCarousel({
+      nav: true,
+      loop: false,
+      dots: true,
+      rtl:true,
+      navText: ['<i class="fa fa-chevron-left"></i>', '<i class="fa fa-chevron-right"></i>'],
+      items: 6,
+      margin: 25,
+      responsive: {
+        0: {
+          items: 2
+        },
+        700: {
+          items: 4
+        },
+        1000: {
+          items: 6,
+        },
+  
+      },
+  
+    });
+  
+    $(".nav-slider").owlCarousel({
+      nav: false,
+      loop: false,
+      dots: false,
+      autoplay: true,
+      items: 2,
+      rtl:true,
+  
+    });
+  
+    $('.success.chart').easyPieChart({
+      size: 68,
+      barColor: "#31D894",
+      scaleLength: 0,
+      lineWidth: 3,
+      trackColor: "#E9E9E9",
+      lineCap: "circle",
+      animate: 2000,
+      rtl:true,
+    });
+    $('.videoChart.chart').easyPieChart({
+      size: 38,
+      barColor: "#FFCE21",
+      scaleLength: 0,
+      lineWidth: 3,
+      trackColor: "#E9E9E9",
+      lineCap: "circle",
+      animate: 2000,
+      rtl:true,
+    });
+    $('.InProgress.chart').easyPieChart({
+      size: 68,
+      barColor: "#FFCE21",
+      scaleLength: 0,
+      lineWidth: 3,
+      trackColor: "#E9E9E9",
+      lineCap: "circle",
+      animate: 2000,
+      rtl:true,
+    });
+  
+    $(window).scroll(function () {
+      scrollTop = $(window).scrollTop();
+      if ((scrollTop + $(window).height()) > $(document).height()) {
+  
+        $(".up").stop(true).animate({
+          opacity: 1
+        }, 100);
+  
+  
+      } else {
+  
+        $(".up").stop(true).animate({
+          opacity: 0
+        }, 250);
+  
+      }
+  
+  
+  
+    });
+  
+  
+    function onScroll(event) {
+      var scrollPosition = $(document).scrollTop();
+      $('.courseDetailsBody .coursepage-tabs .nav-link').each(function () {
+        console.log($(this).attr('href'));
+      });
+    }
+  
+    $('.courseDetailsBody .coursepage-tabs .nav-link').click(function () {
+      var refElement = $(this).attr("href");
+      $('html, body').animate({ scrollTop: $(this.hash).offset().top - 5 }, 1000);
+  
+      $('.courseDetailsBody .coursepage-tabs .nav-item').removeClass("active");
+  
+      $(this).closest('.nav-item').addClass("active");
+  
+      return false;
+  
+    });
+  
+    $(document).on("scroll", onScroll);
+    // alert(scrollTop);
+    $(".up").on("click", function (e) {
+      e.preventDefault()
+      console.log('asdasd');
+      $("html, body").animate({ scrollTop: 0 }, "slow");
+  
+    });
+  
+    $(".langBtn").click(function () {
+      $(".langDropDown").toggle()
+    });
+    $(".showHidePass").click(function () {
+  
+      var x = $(this).closest(".form-group").find(".form-control");
+      if (x.attr("type") == "text") {
+        x.attr("type", "password");
+        $(this).closest(".form-group").find(".showPass").css("display", "block");
+        $(this).closest(".form-group").find(".hidePass").css("display", "none");
+      }
+      else {
+        x.attr("type", "text");
+        $(this).closest(".form-group").find(".hidePass").css("display", "block");
+        $(this).closest(".form-group").find(".showPass").css("display", "none");
+      }
+    });
+    $(".datepicker").datepicker({
+      changeMonth: true,
+      changeYear: true
+    });
+    $(document).on('click', '.dropdown-menu a', function (e) {
+  
+      e.stopPropagation();
+    });
+  
+    // make it as accordion for smaller screens
+  
+    $('.dropdown-menu a').click(function (e) {
+  
+      e.preventDefault();
+      if ($(this).next('.submenu').length) {
+        $(this).addClass("active")
+        $('.submenu').hide();
+        $(this).next('.submenu').css("display", "block");
+      }
+      $('.dropdown').on('hide.bs.dropdown', function () {
+        $(this).find('.submenu').hide();
+      })
+    });
+  
+  
+      $('.select').selectric({
+        disableOnMobile: false,
+        nativeOnMobile: false
+      }
+      );
+    $('.selectric .button').text('');
+  
+    $('.selectric .button').append('<i class="lni lni-chevron-down"></i>');
+    $('.loginLink').click(function () {
+      $('.modal').modal('hide');
+      $('#LoginModal').modal('show')
+    });
+    $('.regBtnLink').click(function () {
+      $('.modal').modal('hide');
+      $('#regModal').modal('show')
+    });
+    $('.forgetLink').click(function () {
+      $('.modal').modal('hide');
+      $('#forgerPassModal').modal('show')
+    });
+    $('.instructortBtn').click(function () {
+      $('.modal').modal('hide');
+      $('#InstructorModal').modal('show')
+    });
+    $(".form-control").click(function () {
+      $(this).attr("placeholder", " ");
+      if ($(this).attr("value") != "") {
+        $(this).attr("value", "")
+      }
+  
+    });
+  
+    $(".slider-range-min").slider({
+      range: "min",
+      value: 65,
+      min: 1,
+      max: 100,
+      slide: function (event, ui) {
+        $(this).parent().find(".amount").val("%" + ui.value);
+      }
+    });
+    $(".amount").val("%" + $(".slider-range-min").slider("value"));
+  
+    $(".slider-range").slider({
+      range: "min",
+      value: 65,
+      max: 100,
+      min: 1
+    });
+  
+    /************************ */
+  
+  
+    /**************FAQ Collapse */
+    $('.accordion-panel').click(function () {
+      $(this).not(this).find('span:first').removeClass('lni-chevron-up').addClass('lni-chevron-down');
+      $(this).find('span:first').toggleClass('lni lni-chevron-up lni lni-chevron-down');
+    })
+    $(".CurriculumList li").click(function () {
+      $(this).not(this).find('span:first').removeClass('lni-chevron-down').addClass('lni-chevron-up');
+      $(this).find('span:first').toggleClass('lni lni-chevron-down lni lni-chevron-up');
+    })
+    $(".NoteCollapse .accordion-panel").click(function () {
+      $(this).not(this).find('span:first').removeClass('lni-chevron-up').addClass('lni-chevron-down');
+      $(this).find('span:first').toggleClass('lni lni-chevron-up lni lni-chevron-down');
+    })
+    $('.faqHeader').click(function () {
+      $(this).parent().find('.faqbody').slideToggle(400);
+    });
+    $(".faqHeader").on("click", "a", function (e) { e.preventDefault() });
+  
+    $('.CurriculumList li').click(function () {
+      $(this).find('.innerCurriculumList').slideToggle(400);
+    });
+    $(".CurriculumList li").on("click", "a", function (e) { e.preventDefault() });
+    $(".radioBtnContainer").on("click", "a", function (e) { e.preventDefault() });
+    $(".NoteCollapse").on("click", "a", function (e) { e.preventDefault() });
+    $(".issueCertificateBtn").click(function () {
+      $(this).css("display", "none");
+      $(this).parent().find(".MyCoursesCertficateBtns").css("display", "block");
+      $(this).parent().parent().find(".MycourseCertificateImg").css("filter", "blur(0)")
+    });
+    $(".readOnlyRating").starRating({
+      starSize: 16,
+      activeColor: '#FFD93F',
+      hoverColor: '#FFD93F',
+      ratedColor: '#FFD93F',
+      emptyColor: 'transparent',
+      starShape: 'straight',
+      strokeColor: '#FFD93F',
+      strokeWidth: 9,
+      ratedColors: '#FFD93F',
+      useGradient: false,
+      readOnly: true,
+      rtl:true,
+      callback: function (currentRating, $el) {
+        // make a server call here
+      }
+    });
+    $(".lectureReviewsReadOnly").starRating({
+      starSize: 16,
+      activeColor: '#FFD93F',
+      hoverColor: '#FFD93F',
+      ratedColor: '#FFD93F',
+      emptyColor: 'transparent',
+      starShape: 'straight',
+      strokeColor: '#FFD93F',
+      strokeWidth: 9,
+      ratedColors: '#FFD93F',
+      useGradient: false,
+      readOnly: true,
+      rtl:true,
+      callback: function (currentRating, $el) {
+        // make a server call here
+      }
+    });
+    $(".learnerReviewsReadOnly").starRating({
+      starSize: 16,
+      rtl: true,
+      activeColor: '#FFB13E',
+      hoverColor: '#FFB13E',
+      ratedColor: '#FFB13E',
+      emptyColor: 'transparent',
+      starShape: 'straight',
+      strokeColor: '#FFB13E',
+      strokeWidth: 9,
+      ratedColors: '#FFD93F',
+      useGradient: false,
+      readOnly: true,
+      callback: function (currentRating, $el) {
+        // make a server call here
+      }
+    });
+    $(".leaveReviewRating").starRating({
+      starSize: 16,
+      activeColor: '#FFB13E',
+      hoverColor: '#FFB13E',
+      ratedColor: '#FFB13E',
+      emptyColor: 'transparent',
+      starShape: 'straight',
+      strokeColor: '#FFB13E',
+      strokeWidth: 9,
+      ratedColors: '#FFD93F',
+      useGradient: false,
+      disableAfterRate: true,
+      useFullStars: true,
+      rtl:true,
+      callback: function (currentRating, $el) {
+        // make a server call here
+      }
+  
+    });
+  
+  });
+  
+  
+  
+  
